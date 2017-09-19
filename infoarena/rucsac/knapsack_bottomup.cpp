4a4400a40ae1677868e3cb78a901fcf48191e755
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2017-09-20 01:00:27 +0300

    Knapsack - bottom-up - 100p

---
+++ infoarena/rucsac/knapsack_bottomup.cpp
@@ -0,0 +1,32 @@
+// Knapsack - bottom-up
+#include <iostream>
+#include <fstream>
+
+int constexpr MAXN = 5001;
+int constexpr MAX_WEIGHT = 10001;
+int objectsNum, maxWeight, globalMax;
+int weights[MAXN], prices[MAXN], revenues[MAX_WEIGHT];
+
+int main() {
+    std::ifstream in("rucsac.in");
+    std::ofstream out("rucsac.out");
+
+    in >> objectsNum >> maxWeight;
+    for (int i = 0; i < objectsNum; ++i) {
+        in >> weights[i] >> prices[i];
+    }
+
+    for (int i = 0; i < objectsNum; ++i) {
+        for (int weight = maxWeight - weights[i]; weight >= 0; --weight) {
+            if (revenues[weight] + prices[i] > revenues[weight + weights[i]]) {
+                revenues[weight + weights[i]] = revenues[weight] + prices[i];
+                if (revenues[weight + weights[i]] > globalMax) {
+                    globalMax = revenues[weight + weights[i]];
+                }
+            }
+        }
+    }
+
+    out << globalMax << "\n";
+    return 0;
+}
